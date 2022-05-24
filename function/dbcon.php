02854a451119f8f9eb5cc43e9d1c30c76c28f620
TANAPAT <68935295+tanapat-unlimitkub@users.noreply.github.com>
2022-05-25 00:11:26 +0700

    Update dbcon.php

--- function/dbcon.php
+++ function/dbcon.php
@@ -1,12 +1,5 @@
 <?php
 
-$host = 'localhost';
-$username = 'viphack1_vip';
-$password = 'dBf4HLqd5B';
-$dbname = 'viphack1_vip';
-
-
-
 try {
     $sqlite = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 } catch (Exception $ex) {
